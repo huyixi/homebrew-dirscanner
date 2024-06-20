be71d1fce3827744e279b923d1618d8f860b8f0b
huyixi <hi@huyixi.com>
2024-06-20 15:09:04 +0800

    Add DirScanner formula

---
+++ Formula/dirscanner.rb
@@ -0,0 +1,15 @@
+class Dirscanner < Formula
+  desc "A CLI program to scan directories and collect file contents"
+  homepage "https://github.com/huyixi/dir-scanner"
+  url "https://github.com/huyixi/dir-scanner/releases/download/v0.0.1/dirscanner-0.0.1.tar.gz"
+  sha256 "4a70527986bffac3c44705d12f5b32325c6c86b134d62590e0054fec8718488a"
+  license "MIT"
+
+  def install
+    bin.install "dirscanner"
+  end
+
+  test do
+    system "#{bin}/dirscanner", "--version"
+  end
+en
