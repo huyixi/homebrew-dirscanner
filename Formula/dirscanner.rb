57eff02adab79f9f1a964d1f0c12ae647c9e4f3f
huyixi <hi@huyixi.com>
2024-06-20 15:16:46 +0800

    Fix syntax error in DirScanner formula

--- Formula/dirscanner.rb
+++ Formula/dirscanner.rb
@@ -1,8 +1,8 @@
 class Dirscanner < Formula
   desc "A CLI program to scan directories and collect file contents"
   homepage "https://github.com/huyixi/dir-scanner"
-  url "https://github.com/huyixi/dir-scanner/releases/download/v0.0.1/dir-scanner-0.0.1.tar.gz"
-  sha256 "4a70527986bffac3c44705d12f5b32325c6c86b134d62590e0054fec8718488a"
+  url "https://github.com/huyixi/dir-scanner/releases/download/v0.0.1/dirscanner-0.0.1.tar.gz"
+  sha256 "3257791b9323e25d2b9176c0a4f0010aea6f9579ad2d1631ac1a02e0e0c41868"
   license "MIT"
 
   def install
